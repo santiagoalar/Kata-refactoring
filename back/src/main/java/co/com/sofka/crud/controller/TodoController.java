e986823a786c6cc960b7da5820d9b9a97a03eb98
Santiago Alarcon <santiago-alar@hotmail.com>
2021-06-03 00:56:35 -0500

    Implement todoDto in TodoController and change Iterable for List

--- back/src/main/java/co/com/sofka/crud/controller/TodoController.java
+++ back/src/main/java/co/com/sofka/crud/controller/TodoController.java
@@ -1,41 +1,51 @@
 package co.com.sofka.crud.controller;
 
+import co.com.sofka.crud.TodoMapper;
+import co.com.sofka.crud.dto.TodoDto;
 import co.com.sofka.crud.service.TodoService;
 import co.com.sofka.crud.entity.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("api")
 public class TodoController {
 
     @Autowired
     private TodoService service;
 
-    @GetMapping(value = "api/todos")
-    public Iterable<Todo> list(){
-        return service.list();
+    @Autowired
+    private TodoMapper todoMapper;
+
+    @GetMapping(value = "todos")
+    public List<TodoDto> list(){
+        return todoMapper.toDtoList((List<Todo>) service.list());
     }
     
-    @PostMapping(value = "api/todo")
-    public Todo save(@RequestBody Todo todo){
-        return service.save(todo);
+    @PostMapping(value = "todo")
+    public TodoDto save(@RequestBody TodoDto todoDto){
+        Todo todo = service.save(todoMapper.todoDtoToTodo(todoDto));
+        return todoMapper.todoToTodoDto(todo);
     }
 
-    @PutMapping(value = "api/todo")
-    public Todo update(@RequestBody Todo todo){
-        if(todo.getId() != null){
-            return service.save(todo);
+    @PutMapping(value = "todo")
+    public TodoDto update(@RequestBody TodoDto todoDto){
+        if(todoDto.getId() != null){
+            Todo todo = service.save(todoMapper.todoDtoToTodo(todoDto));
+            return todoMapper.todoToTodoDto(todo);
         }
-        throw new RuntimeException("No existe el id para actualziar");
+        throw new RuntimeException("No existe el id para actualizar");
     }
 
-    @DeleteMapping(value = "api/{id}/todo")
+    @DeleteMapping(value = "{id}/todo")
     public void delete(@PathVariable("id")Long id){
         service.delete(id);
     }
 
-    @GetMapping(value = "api/{id}/todo")
+    @GetMapping(value = "{id}/todo")
     public Todo get(@PathVariable("id") Long id){
         return service.get(id);
     }
