7ff34943b4c35cb7ef8588a5062bcdb35ea57b48
Santiago Alarcon <santiago-alar@hotmail.com>
2021-06-04 21:37:08 -0500

    add some styles

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {StoreProvider} from './StoreProvider';
+import { StoreProvider } from './StoreProvider';
 import FormParent from './FormParent';
 import ListParent from './ListParent';
 
 export const HOST_API = "http://localhost:8080/api";
 
+
 function App() {
-  return <StoreProvider>
-    <h3>To-Do List</h3>
-    <FormParent />
-    <ListParent />
-  </StoreProvider>
+    return <StoreProvider>
+      <h3>To-Do List</h3>
+        <FormParent />
+        <ListParent />
+
+    </StoreProvider>
 }
 
 export default App;
